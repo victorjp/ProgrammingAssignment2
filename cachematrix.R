defac8fd57b379e5ef7538f48e8af6c977315206
victorjp <victor.jaen.panaitescu@gmail.com>
2015-02-22 16:02:31 +0000

    Final version
    including comments and validation of the original state of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,69 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# This function takes a matrix as an argument, and creates
+# a "special" matrix element, that can cache its inverse
+# by calling the CalcInverse function.
+# It is assumed that the matrix is invertible (square and determinant <> 0)
 
 makeCacheMatrix <- function(x = matrix()) {
   
+  inverse <- NULL
+  initialstate <<-  TRUE #pointer to validate if the matrix has changed
 
-## can get the inverse matrix and cache's it
-## create a square matrix
-  
-## creates the inverse variable argument  
-inverse <- NULL
-
-CreateMatrix <- function(v) {
-    x <<-v # passes the matrix initially pased to the makeCahce and creates a pointer
-           # in the cache 
-    inverse <<-NULL  #same for the inverse pointer
+  #caches the matrix to a pointer, by default is the original matriz
+  #but it could cache a different matrix
+  CacheMe <- function(v = x) {
+    if (!identical(x,v)) initialstate <<- FALSE
+    x <<-v            
+    inverse <<-NULL
   }
-PrintMatrix <- function() x
-
-CalcInverse <- function() {
-    ## If (det(x)==0) message("Matrix non invertible")
-    inverse <<- solve(x)  #gets t
+  #returns the matrix
+  PrintMe <- function() x
   
+  #obtains the inverse of the matrix and stores it in cache
+  InverseMe <- function() {
+    inverse <<- solve(x)
   }
-PrintInverse <- function() inverse
+  #returns the inverse of the matrix already stored
+  PrintMeInverse <- function() inverse
 
-#output of the CreateMatrix function
-list(CreateMatrix = CreateMatrix, PrintMatrix = PrintMatrix,
-     CalcInverse = CalcInverse,
-     PrintInverse = PrintInverse)  
-
-
-  
+  #output of the primary function
+  list(CacheMe = CacheMe, PrintMe = PrintMe,
+       InverseMe = InverseMe,
+       PrintMeInverse = PrintMeInverse)  
+    
 }
 
-## Write a short comment describing this function
+
+# This function receive a special matrix element as created by the function above
+# and verifies if the inverse is already stored in the memory and the matrix
+# is the same that was original passed when the special matrix elemented was created.
+# If both conditions are met, then it returns the inverse of the matrix already stored
+# in cache. Otherwise it will calculated the inverse and store it in cache.
 
 cacheSolve <- function(x, ...) {
   
-
-## will get the inverse of the Matrix created above, but first it will validat
-## is the matrix is the same and the inverse already exist, it would returned
-# the cache one, otherwise it will do the math
-  
- # if (identical(y,x)==FALSE) {
-    
-#  mesage("Matrix changed, impossible to retrieve from cache")  
-    
- # }  This condition is for the matriz change or not
-  
-  if (!is.null(x$PrintInverse())) {
-    message("getting cached matrix")
-    x$PrintInverse()
-    
-    
+  #This if verifies that the inverse is cached and it is still the initial matrix  
+  if (!is.null(x$PrintMeInverse()) && initialstate == TRUE) {
+    message("getting cached matrix") 
+    x$PrintMeInverse()  # returns the cached inverse
   }
+  #If the conditions above are not met, then it calculated the inverse
+  #and stores it in the cache.
   else  {
-    x$CalcInverse()
-    x$PrintInverse()
+    initialstate <<- TRUE
+    x$InverseMe()
+    x$PrintMeInverse()  
   }
   
-    
-    
-
-  
-  ## Return a matrix that is the inverse of 'x'
 }
